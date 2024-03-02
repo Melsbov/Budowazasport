c95f2b5ac5b026a864cdda71ff08855b5777e200
Melsbov <144225573+Melsbov@users.noreply.github.com>
2024-03-02 22:05:39 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var locaties = ["schagen", "callantsoog", "tzand", "petten", "annapaulowna"];
+    var locaties = ["schagen", "callantsoog", "tzand", "petten", "annapaulowna", "submenu1"];
 
     locaties.forEach(function(locatie) {
         var menuItems = document.querySelectorAll("#" + locatie );
 
         menuItems.forEach(function(menuItem) {
             menuItem.addEventListener("click", function() {
+                console.log("clicked")
                 this.classList.toggle("active");
             });
         });
